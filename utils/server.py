20d461188db2e3ea55194df119a2eb1caab07c09
Akhil.A.S <119261400+wagger1@users.noreply.github.com>
2025-06-24 12:38:47 +0530

    Update server.py

--- utils/server.py
+++ utils/server.py
@@ -21,8 +21,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-async def route():
+def route():
     return "<h1>Check <a href='https://github.com/arun017s/AutoDelete'>AutoDelete</a></h1>"
 
 if __name__ == "__main__":     
-   app.run()
+   app.run(host="0.0.0.0", port=8080)
